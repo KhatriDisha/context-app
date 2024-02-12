58edcb627693e31237f8d990ac26321a5c4ec148
kdisha <disha.khatri@indiqus.com>
2024-02-12 17:35:47 +0530

    react context api  with classes and functional components

---
+++ src/component/BookList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+class BookList extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className='book-list'>
+                <ul>
+                    <li>hello how are you ?</li>
+                    <li>I am good</li>
+                    <li>Cool</li>
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default BookList;
